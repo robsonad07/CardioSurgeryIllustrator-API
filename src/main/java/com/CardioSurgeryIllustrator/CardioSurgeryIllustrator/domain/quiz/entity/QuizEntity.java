4a9b74a8e41e3b3c8102be66e19765c22e2c9270
Robson <robsonad07@gmail.com>
2025-01-25 18:30:57 -0300

    QuizEntity updated

--- src/main/java/com/CardioSurgeryIllustrator/CardioSurgeryIllustrator/domain/quiz/entity/QuizEntity.java
+++ src/main/java/com/CardioSurgeryIllustrator/CardioSurgeryIllustrator/domain/quiz/entity/QuizEntity.java
@@ -1,8 +1,11 @@
 package com.CardioSurgeryIllustrator.CardioSurgeryIllustrator.domain.quiz.entity;
 
+import com.CardioSurgeryIllustrator.CardioSurgeryIllustrator.domain.question.entity.QuestionEntity;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity(name = "Quiz")
@@ -12,6 +15,11 @@ public class QuizEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Column(unique = true)
     private String title;
+    @Column(nullable = false)
     private String description;
+    @OneToMany(mappedBy = "quizEntity", cascade = CascadeType.REMOVE)
+    @JsonManagedReference
+    private List<QuestionEntity> questionEntityList;
 }
